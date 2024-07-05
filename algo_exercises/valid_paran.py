52e7bb3953444b996059b4cd78662391aa47b734
Han Lin <hanxlin@users.noreply.github.com>
2024-07-05 15:35:11 -0700

    update algo exercise

--- algo_exercises/valid_paran.py
+++ algo_exercises/valid_paran.py
@@ -10,28 +10,22 @@ lookup = {
 def _rec(l, m, n, stack, accum, ans):
     # base case
     if l == 0 and m == 0 and n == 0:
-        while len(stack) > 0:
-            accum += lookup[stack.pop()]
+        accum += ''.join([lookup[op] for op in stack[::-1]])
         ans.append(accum)
+        return
 
     # all the options as branches
     # each branch must have its own state, ie. stack needs to be copied
+    if l > 0:
+        _rec(l-1, m, n, stack+['('], accum+'(', ans)
+    if m > 0:
+        _rec(l, m-1, n, stack+['<'], accum+'<', ans)
+    if n > 0:
+        _rec(l, m, n-1, stack+['{'], accum+'{', ans)
     if len(stack) > 0:
         stack_cp = stack[:]
         op = stack_cp.pop()
         _rec(l, m, n, stack_cp, accum+lookup[op], ans)
-    if l > 0:
-        stack_cp = stack[:]
-        stack_cp.append('(')
-        _rec(l-1, m, n, stack_cp, accum+'(', ans)
-    if m > 0:
-        stack_cp = stack[:]
-        stack_cp.append('<')
-        _rec(l, m-1, n, stack_cp, accum+'<', ans)
-    if n > 0:
-        stack_cp = stack[:]
-        stack_cp.append('{')
-        _rec(l, m, n-1, stack_cp, accum+'{', ans)
 
 def solve(l, m, n):
     ans = []
